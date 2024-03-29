c56654dfc9103d97cdd7675417ff33b6b2615312
BboyJayJ <121296138+BboyJayJ@users.noreply.github.com>
2024-03-29 22:40:11 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -11,6 +11,43 @@ $(document).ready(function () {
         } else {
             hyperlink.fadeOut(1500);
         }
+
+        /*fly-in EF1*/
+        var $element = $('.content');
+        var positionFromTop = $element.offset().top;
+        var windowHeight = $(window).height();
+        var triggerPoint = positionFromTop - windowHeight + 200; // 觸發動畫滾動位置
+
+        if ($(window).scrollTop() > triggerPoint) {
+            $element.addClass('active');
+        }
+
+        /*fly-in EF2*/
+        var $element = $('.img-EF');
+        var positionFromTop = $element.offset().top;
+        var windowHeight = $(window).height();
+        var triggerPoint = positionFromTop - windowHeight + 200; // 觸發動畫滾動位置
+
+        if ($(window).scrollTop() > triggerPoint) {
+            $element.addClass('active1');
+        }
     });
     $(window).scroll();
+
+    /*slogan ef*/
+    anime.timeline({ loop: true })
+        .add({
+            targets: '.ml15 .word',
+            scale: [14, 1],
+            opacity: [0, 1],
+            easing: "easeOutCirc",
+            duration: 800,
+            delay: (el, i) => 800 * i
+        }).add({
+            targets: '.ml15',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
 });
